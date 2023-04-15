1f3aa80b452a927cc4d49318916d1b802794f2a6
Ahmad-Fadhil-Aulia-Faisal <ahmadfadhilauliafaisal@gmail.com>
2023-04-15 20:38:27 +0700

    Penyesuaian dan perapihan
    Menghapus seluruh komentar yang terdapat pada TaxFunction.java
    
    -Mengubah nilai konstan yang digunakan dalam penghitungan pajak menjadi private static final variabel di kelas.
    -Menambahkan penanganan exception jika jumlah bulan bekerja lebih dari 12 atau jumlah anak negatif.
    -Menghitung jumlah penghasilan tidak kena pajak dan pemotongan dalam variabel baru.
    -Menggunakan variabel baru dalam penghitungan pajak dan memastikan bahwa pajak tidak bisa negatif dengan menggunakan Math.max() pada nilai kembalian.

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -2,43 +2,29 @@ package lib;
 
 public class TaxFunction {
 
-	
-	/**
-	 * Fungsi untuk menghitung jumlah pajak penghasilan pegawai yang harus dibayarkan setahun.
-	 * 
-	 * Pajak dihitung sebagai 5% dari penghasilan bersih tahunan (gaji dan pemasukan bulanan lainnya dikalikan jumlah bulan bekerja dikurangi pemotongan) dikurangi penghasilan tidak kena pajak.
-	 * 
-	 * Jika pegawai belum menikah dan belum punya anak maka penghasilan tidak kena pajaknya adalah Rp 54.000.000.
-	 * Jika pegawai sudah menikah maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000.
-	 * Jika pegawai sudah memiliki anak maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000 per anak sampai anak ketiga.
-	 * 
-	 */
-	
-	
-	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
-		
-		int tax = 0;
-		
-		if (numberOfMonthWorking > 12) {
-			System.err.println("More than 12 month working per year");
-		}
-		
-		if (numberOfChildren > 3) {
-			numberOfChildren = 3;
-		}
-		
-		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
-		}else {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
-		}
-		
-		if (tax < 0) {
-			return 0;
-		}else {
-			return tax;
-		}
-			 
-	}
-	
+    private static final int DEFAULT_TAX_DEDUCTIBLE = 54000000;
+    private static final int MARRIED_TAX_DEDUCTIBLE = DEFAULT_TAX_DEDUCTIBLE + 4500000;
+    private static final int CHILD_TAX_DEDUCTIBLE = 1500000;
+    private static final int MAX_CHILDREN = 3;
+
+
+    public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
+        if (numberOfMonthWorking > 12) {
+            throw new IllegalArgumentException("Number of working months can not exceed 12");
+        }
+        if (numberOfChildren < 0) {
+            throw new IllegalArgumentException("Number of children can not be negative");
+        }
+        
+        int childDeductible = Math.min(numberOfChildren, MAX_CHILDREN) * CHILD_TAX_DEDUCTIBLE;
+        int taxDeductible = DEFAULT_TAX_DEDUCTIBLE;
+        if (isMarried) {
+            taxDeductible = MARRIED_TAX_DEDUCTIBLE;
+        }
+        
+        int taxableIncome = (monthlySalary + otherMonthlyIncome) * numberOfMonthWorking - deductible - taxDeductible - childDeductible;
+        int tax = (int) Math.round(0.05 * taxableIncome);
+        return Math.max(tax, 0);
+    }
+    
 }
